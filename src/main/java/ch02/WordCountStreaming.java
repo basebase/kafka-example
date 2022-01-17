0849fee9b4af76f7ec1eafd26e49122467d8a0b0
jianfeng.li <base_2@163.com>
2022-01-17 10:47:58 +0800

    kafka stream wordcount处理程序

--- src/main/java/ch02/WordCountStreaming.java
+++ src/main/java/ch02/WordCountStreaming.java
@@ -2,23 +2,26 @@ package ch02;
 
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.*;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
 /**
  * @Author xiaomoyu
- * @Date: 2022/1/12 11:33:33
- * @Description:    利用kafka实现wordcount流式应用
+ * @Date: 2022/1/14 15:34:54
+ * @Description:   利用kafka实现wordcount流式应用
+ *
  *
  *
  * ./kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic java-example-source
  * ./kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic java-example-output
+ *
+ *
  *
  *
  * ./kafka-topics.sh --bootstrap-server localhost:9092 \
@@ -28,72 +31,79 @@ import java.util.Properties;
  * ./kafka-topics.sh --bootstrap-server localhost:9092 \
  * --create --partitions 1 --replication-factor 1 --topic java-example-output
  *
+ *
  * ./kafka-console-producer.sh --bootstrap-server localhost:9092 --topic java-example-source
  *
- * ./kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic java-example-output
+ * ./kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic java-example-output \
+ * --property print.key=true \
+ * --property value.deserializer=org.apache.kafka.common.serialization.LongDeserializer
+ *
  *
  */
 public class WordCountStreaming {
     public static void main(String[] args) {
+        // 1. 初始化配置
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-word-count-example");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class);
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.LongSerde.class);
-        String sourceTopic = "java-example-source";
-        String targetTopic = "java-example-output";
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "word-count-app");
+        props.put(StreamsConfig.CLIENT_ID_CONFIG, "word-count-client");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
+        // 测试用: 禁止使用缓存
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+
+        // 配置状态目录, 完成测试后可以删除
+        props.put(StreamsConfig.STATE_DIR_CONFIG, "/Users/xiaomoyu/Documents/code/java/kafka-example/stream-state");
+
+        // 2. 创建StreamsBuilder
         StreamsBuilder builder = new StreamsBuilder();
-        // 这里需要指定类型, 否则会报错
-        // https://stackoverflow.com/questions/62115660/kafka-streams-exception-org-apache-kafka-streams-errors-streamsexception-dese
-        KStream<String, String> source = builder.stream(sourceTopic, Consumed.with(Serdes.String(), Serdes.String()));
 
-        KStream<String, Long> flatMapResult = source.flatMap(new KeyValueMapper<String, String, List<KeyValue<String, Long>>>() {
+
+
+        // 3. 逻辑处理
+
+        // 3.1 创建stream指定kv的序列化
+        KStream<String, String> stream =
+                builder.stream("java-example-source", Consumed.with(Serdes.String(), Serdes.String()));
+
+
+        // 3.2 数据打平
+        KStream<String, String> flatMapValue = stream.flatMapValues(new ValueMapper<String, List<String>>() {
             @Override
-            public List<KeyValue<String, Long>> apply(String key, String value) {
-                String[] datas = value.split(",");
-                List<KeyValue<String, Long>> resultList = new ArrayList<>();
-                for (String data : datas) {
-                    if (data.equals("") || data == null)
-                        continue;
-                    resultList.add(new KeyValue<>(data, 1L));
-                }
-                return resultList;
+            public List<String> apply(String value) {
+                String[] values = value.split(",");
+                return Arrays.asList(values);
             }
         });
 
-        KGroupedStream<String, Long> groupedStream = flatMapResult.groupBy(new KeyValueMapper<String, Long, String>() {
-
+        // 3.3 数据分组
+        KGroupedStream<String, String> groupValue = flatMapValue.groupBy(new KeyValueMapper<String, String, String>() {
             @Override
-            public String apply(String key, Long value) {
-                StringBuffer buffer = new StringBuffer();
-                buffer.append("key: ").append(key).append("===")
-                        .append("value: ").append(value);
-                System.out.println(buffer);
-                return key;
+            public String apply(String key, String value) {
+                System.out.println("key: " + key + " value: " + value);
+                return value;
             }
         });
 
 
-        groupedStream.aggregate(new Initializer<Long>() {
-            @Override
-            public Long apply() {
-                return 0L;
-            }
-        }, new Aggregator<String, Long, Long>() {
-            @Override
-            public Long apply(String key, Long value, Long aggregate) {
-                System.out.println("key: " + key + " value: " + value + " aggregate: " + aggregate);
-                return value + aggregate;
-            }
-        }).toStream().to(targetTopic);
+        // 3.4 数据聚合
+        KTable<String, Long> table = groupValue.count();
+
+        System.out.println("table: " + table);
 
 
+        // 需要在这里进行转换, 否则写不进指定的分区
+        KStream<String, Long> out = table.toStream(Named.as("java-example-stream-out"));
+
+        out.to("java-example-output", Produced.with(Serdes.String(), Serdes.Long()));
+        out.print(Printed.<String, Long>toSysOut().withLabel("wordcount"));
 
 
-
-
-        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        Topology topology = builder.build();
+        KafkaStreams streams = new KafkaStreams(topology, props);
+        streams.cleanUp();
         streams.start();
+        System.out.println(topology.describe());
     }
 }
